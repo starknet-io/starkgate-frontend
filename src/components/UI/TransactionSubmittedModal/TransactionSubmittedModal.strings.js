7ae4c19f92a67a3e5c934dfb695b34334cc20fd2
Dan Ziv <danz@starkware.co>
2021-12-20 23:31:22 +0200

    chore: remove hooks plugin from eslint [skip ci]

--- src/components/UI/TransactionSubmittedModal/TransactionSubmittedModal.strings.js
+++ src/components/UI/TransactionSubmittedModal/TransactionSubmittedModal.strings.js
@@ -1,5 +1,5 @@
 export const BTN_TEXT = explorer => `view on ${explorer}`;
 export const BODY_TXT_PARTS = [
   `Your transaction has been sent to StarkNet!\n`,
-  `We'll notify you once it's status will be updated.`
+  `We'll notify you once its status will be updated.`
 ];
