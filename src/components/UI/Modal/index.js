a7bc81c76564bcad3cfbae45967c6e64befe74db
Dan Ziv <danz@starkware.co>
2021-12-22 18:56:40 +0200

    --amend

---
+++ src/components/UI/Modal/index.js
@@ -0,0 +1,5 @@
+export * from './Modal/Modal';
+export * from './ModalHeader/ModalHeader';
+export * from './ModalBody/ModalBody';
+export * from './ModalTitle/ModalTitle';
+export * from './ModalFooter/ModalFooter';
---
+++ src/components/UI/Toast/index.js
@@ -0,0 +1,4 @@
+export * from './ToastBody/ToastBody'
+export * from './ToastButton/ToastButton'
+export * from './ToastHeader/ToastHeader'
+export * from './ToastSeparator/ToastSeparator'
