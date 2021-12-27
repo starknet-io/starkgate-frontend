361474302c945ef65c6ca478102d45cb578b4c4b
Dan Ziv <danz@starkware.co>
2021-12-27 17:49:51 +0200

    fix: show txs links before ACCEPTED_ON_L1 (#44)

--- src/enums/TransactionStatus.js
+++ src/enums/TransactionStatus.js
@@ -6,3 +6,18 @@ export const TransactionStatus = {
   ACCEPTED_ON_L2: 'ACCEPTED_ON_L2',
   ACCEPTED_ON_L1: 'ACCEPTED_ON_L1'
 };
+
+export const TransactionPendingStatuses = [
+  TransactionStatus.NOT_RECEIVED,
+  TransactionStatus.RECEIVED
+];
+
+export const TransactionCompletedStatuses = [
+  TransactionStatus.REJECTED,
+  TransactionStatus.ACCEPTED_ON_L1
+];
+
+export const TransactionConsumedStatuses = [
+  TransactionStatus.ACCEPTED_ON_L2,
+  TransactionStatus.PENDING
+];
