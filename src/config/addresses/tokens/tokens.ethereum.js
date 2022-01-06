ee5c754c85838d43e033dafd85492e911f63f95b
Dan Ziv <danz@starkware.co>
2022-01-06 14:47:59 +0200

    Update tokens.ethereum.js

--- src/config/addresses/tokens/tokens.ethereum.js
+++ src/config/addresses/tokens/tokens.ethereum.js
@@ -11,19 +11,6 @@ export const EthereumTokens = [
     }
   },
   {
-    name: 'Mock ERC20 Token',
-    symbol: 'TEST',
-    decimals: 18,
-    bridgeAddress: {
-      [ChainType.MAIN.id]: '',
-      [ChainType.GOERLI.id]: '0x5542fd7dc64008978B50Ccb56274a00DEF2B57FE'
-    },
-    tokenAddress: {
-      [ChainType.MAIN.id]: '',
-      [ChainType.GOERLI.id]: '0xe4C2493C66e068D65Cf1C38a2d7bDa25d9f08980'
-    }
-  },
-  {
     name: 'Wrapped BTC',
     symbol: 'WBTC',
     decimals: 8,
