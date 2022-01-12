769ee593217b364cf5cdc96cfaeca0bce8ecbcea
Dan Ziv <danz@starkware.co>
2022-01-12 21:31:54 +0200

    update abis

--- src/abis/l1/index.js
+++ src/abis/l1/index.js
@@ -1,6 +1,6 @@
-import L1_ERC20_BRIDGE_ABI from './erc20-bridge.json';
-import L1_ERC20_ABI from './erc20.json';
-import L1_ETH_BRIDGE_ABI from './eth-bridge.json';
-import L1_MESSAGING_ABI from './messaging.json';
+import L1_ERC20_ABI from './ERC20.json';
+import L1_ERC20_BRIDGE_ABI from './StarknetERC20Bridge.json';
+import L1_ETH_BRIDGE_ABI from './StarknetEthBridge.json';
+import L1_MESSAGING_ABI from './StarknetMessaging.json';
 
 export {L1_ERC20_ABI, L1_ERC20_BRIDGE_ABI, L1_ETH_BRIDGE_ABI, L1_MESSAGING_ABI};
