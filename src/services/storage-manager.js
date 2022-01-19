fd34f567823642c978914aa8188fd5403675193c
Dan Ziv <danz@starkware.co>
2022-01-19 18:51:59 +0200

    tests

--- src/services/storage-manager.js
+++ src/services/storage-manager.js
@@ -1,40 +1,3 @@
-function b64e(str) {
-  return btoa(
-    encodeURIComponent(str).replace(/%([0-9A-F]{2})/g, function (match, p1) {
-      return String.fromCharCode('0x' + p1);
-    })
-  );
-}
-
-function b64d(str) {
-  return decodeURIComponent(
-    Array.prototype.map
-      .call(atob(str), function (c) {
-        return '%' + c.charCodeAt(0).toString(16);
-      })
-      .join('')
-  );
-}
-
-Storage.prototype.setObjectHash = function (key, myObject) {
-  const newObject = {};
-  Object.keys(myObject).map(function (value) {
-    newObject[value] = b64e(myObject[value]);
-  });
-  this.setItem(key, b64e(JSON.stringify(newObject)));
-};
-
-Storage.prototype.getObjectHash = function (key) {
-  const myObject = this.getItem(key);
-  if (!myObject) return null;
-  return (
-    b64d(myObject) &&
-    JSON.parse(b64d(myObject), function (key) {
-      return key ? b64d(this[key]) : this[key];
-    })
-  );
-};
-
 export const StorageManager = {
   setItem: (key, item) => {
     if (localStorage) {
@@ -53,4 +16,46 @@ export const StorageManager = {
       }
     }
   }
+};
+
+Storage.prototype.setObjectHash = function (key, myObject) {
+  const newObject = {};
+  Object.keys(myObject).map(function (value) {
+    newObject[value] = b64e(myObject[value]);
+  });
+  this.setItem(key, b64e(JSON.stringify(newObject)));
+};
+
+Storage.prototype.getObjectHash = function (key) {
+  let myObject = this.getItem(key);
+  if (!myObject) return null;
+  // For backward compatibility
+  if (myObject.startsWith('[') && myObject.endsWith(']')) {
+    this.setObjectHash(key, myObject);
+    myObject = this.getItem(key);
+  }
+  return (
+    b64d(myObject) &&
+    JSON.parse(b64d(myObject), function (key) {
+      return key ? b64d(this[key]) : this[key];
+    })
+  );
+};
+
+const b64e = function (str) {
+  return btoa(
+    encodeURIComponent(str).replace(/%([0-9A-F]{2})/g, function (match, p1) {
+      return String.fromCharCode('0x' + p1);
+    })
+  );
+};
+
+const b64d = function (str) {
+  return decodeURIComponent(
+    Array.prototype.map
+      .call(atob(str), function (c) {
+        return '%' + c.charCodeAt(0).toString(16);
+      })
+      .join('')
+  );
 };
