76faa39371d2134a2fa7bf1b8a06030a0be9cc91
CarmitKl <96685244+CarmitKl@users.noreply.github.com>
2022-03-29 14:18:10 +0300

    feat: onboarding modal (#109)

--- src/components/Features/Bridge/Bridge.js
+++ src/components/Features/Bridge/Bridge.js
@@ -1,12 +1,35 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 
 import {Account, Faq, SelectToken, ToastProvider, Transfer} from '..';
+import envs from '../../../config/envs';
 import {MenuType} from '../../../enums';
 import {useMenu} from '../../../providers/MenuProvider';
+import {useOnboardingModal} from '../../../providers/ModalProvider';
+import utils from '../../../utils';
 import styles from './Bridge.module.scss';
+
+const {localStorageOnboardingExpirationTimestampKey, onboardingModalTimeoutHrs} = envs;
 
 export const Bridge = () => {
   const {menu, menuProps} = useMenu();
+  const showOnboardingModal = useOnboardingModal();
+
+  useEffect(() => {
+    maybeShowOnboardingModal();
+  }, []);
+
+  const maybeShowOnboardingModal = () => {
+    const onboardingTimestamp = utils.storage.getItem(localStorageOnboardingExpirationTimestampKey);
+    const now = Date.now();
+    const onboardingModalTimeoutMs = utils.date.getMsFromHrs(onboardingModalTimeoutHrs);
+    if (!onboardingTimestamp || onboardingTimestamp < now) {
+      showOnboardingModal();
+      utils.storage.setItem(
+        localStorageOnboardingExpirationTimestampKey,
+        now + onboardingModalTimeoutMs
+      );
+    }
+  };
 
   const renderMenu = () => {
     switch (menu) {
