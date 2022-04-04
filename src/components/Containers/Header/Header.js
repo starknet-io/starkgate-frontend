47a9ac3d74b5f96b3c684bc6179e00fc74992938
Joshua Armstrong <joshua@banxa.com>
2022-04-04 10:32:16 +1000

    Reverted the utils destructuring

--- src/components/Containers/Header/Header.js
+++ src/components/Containers/Header/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import {ReactComponent as StarkGateLogo} from '../../../assets/img/starkgate.svg';
 import {useL1Wallet, useL2Wallet, useWallets} from '../../../providers/WalletsProvider';
-import {toClasses} from '../../../utils';
+import utils from '../../../utils';
 import {useBridgeActions} from '../../Features/Bridge/Bridge.hooks';
 import {useIsL1, useIsL2} from '../../Features/Transfer/Transfer.hooks';
 import {WalletButton} from '../../UI';
@@ -37,17 +37,17 @@ export const Header = () => {
   };
 
   return (
-    <div className={toClasses(styles.header, 'row')}>
-      <div className={toClasses(styles.left, 'row')}>
-        <div className={toClasses(styles.logo, 'row')} onClick={onLogoClick}>
+    <div className={utils.object.toClasses(styles.header, 'row')}>
+      <div className={utils.object.toClasses(styles.left, 'row')}>
+        <div className={utils.object.toClasses(styles.logo, 'row')} onClick={onLogoClick}>
           <StarkGateLogo />
         </div>
         {isConnected && (
-          <div className={toClasses(styles.chain, 'row')}>{CHAIN_TXT(chainName)}</div>
+          <div className={utils.object.toClasses(styles.chain, 'row')}>{CHAIN_TXT(chainName)}</div>
         )}
       </div>
 
-      <div className={toClasses(styles.right, 'row')}>
+      <div className={utils.object.toClasses(styles.right, 'row')}>
         <BuyButton
           btnTxt="Buy with Banxa"
           logoPath={'payments/Banxa/logo.svg'}
