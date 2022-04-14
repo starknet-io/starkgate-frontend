4cff09572918363a745a0921594d7e9a5adee647
Dan Ziv <danz@starkware.co>
2022-04-14 11:23:01 +0300

    Fix

--- src/routes/Terms/Terms.strings.js
+++ src/routes/Terms/Terms.strings.js
@@ -1,7 +1,6 @@
 import utils from '../../utils';
 
-const {title_txt, accept_btn_txt, last_revised_txt, notes_title_txt} =
-  utils.getTranslation('screens.terms');
+const {title_txt, accept_btn_txt, last_revised_txt} = utils.getTranslation('screens.terms');
 
 export const TITLE_TXT = title_txt;
 export const ACCEPT_BTN_TXT = accept_btn_txt;
