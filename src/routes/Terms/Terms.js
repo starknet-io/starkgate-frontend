ba4c2c56e6a00e5f0e76f6590c2334935235d5aa
Dan Ziv <danz@starkware.co>
2022-04-18 14:21:44 +0300

    remove notes

--- src/routes/Terms/Terms.js
+++ src/routes/Terms/Terms.js
@@ -525,25 +525,6 @@ export const Terms = () => {
             </li>
           </ol>
         </div>
-        <div className={styles.notes}>
-          <h3>{notesTitleTxt}</h3>
-          <hr />
-          <ol>
-            <li>
-              As the StarkNet becomes decentralized we may consider removing the Bridge from these
-              terms and taking the position that it is completely decentralized
-            </li>
-            <li>
-              TBD whether we could add a sentence that Starkware also cannot control or direct the
-              operation of the part of the Bridge that sits on the StarkNet in the pre-Universe
-              phase.
-            </li>
-            <li>
-              To discuss the extent to which we can make this statement, or can modify it for
-              accuracy.
-            </li>
-          </ol>
-        </div>
       </div>
       {!isAcceptTerms && (
         <div ref={acceptButtonRef} className={styles.acceptButton}>
