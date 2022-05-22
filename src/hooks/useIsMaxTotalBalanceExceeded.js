3e570d700c87a2096633d6a8eb40774d47b16465
Dan Ziv <danz@starkware.co>
2022-05-22 17:43:50 +0300

    fix

--- src/hooks/useIsMaxTotalBalanceExceeded.js
+++ src/hooks/useIsMaxTotalBalanceExceeded.js
@@ -1,6 +1,6 @@
 import {useCallback} from 'react';
 
-import {useSelectedToken} from '../providers/TransferProvider';
+import {useSelectedToken, useTransfer} from '../providers/TransferProvider';
 import {useL1TokenContract} from './useContract';
 import {useMaxTotalBalance} from './useMaxTotalBalance';
 import {useL1TokenBalance} from './useTokenBalance';
@@ -10,14 +10,15 @@ export const useIsMaxTotalBalanceExceeded = () => {
   const maxTotalBalance = useMaxTotalBalance();
   const getTokenContract = useL1TokenContract();
   const getTokenBridgeBalance = useL1TokenBalance(selectedToken?.bridgeAddress);
+  const {isL1} = useTransfer();
 
   return useCallback(
     async (amount = 0) => {
-      if (maxTotalBalance) {
+      if (maxTotalBalance && isL1) {
         const tokenBridgeBalance = await getTokenBridgeBalance(selectedToken);
         return maxTotalBalance < tokenBridgeBalance + Number(amount);
       }
     },
-    [getTokenContract, selectedToken, maxTotalBalance, getTokenBridgeBalance]
+    [getTokenContract, selectedToken, maxTotalBalance, isL1, getTokenBridgeBalance]
   );
 };
