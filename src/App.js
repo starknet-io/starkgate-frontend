0570a922b90a7069e7b71c14bbc75b707fb34c54
Dan Ziv <danz@starkware.co>
2022-05-23 09:28:21 +0300

    feat: move the discord tab to a side button (#199)

--- src/App.js
+++ src/App.js
@@ -2,22 +2,34 @@ import React from 'react';
 import {Navigate, Route, Routes, useLocation} from 'react-router-dom';
 
 import styles from './App.module.scss';
+import {TrackEvent} from './analytics';
+import {ReactComponent as DiscordIcon} from './assets/svg/icons/discord.svg';
 import {Footer, Header} from './components/Containers';
 import {StyledBackground} from './components/UI';
-import {useBuyProviders} from './hooks';
+import {SideButton} from './components/UI/SideButton/SideButton';
+import {useBuyProviders, useConstants, useTracking} from './hooks';
 import {useApp, useLogin} from './providers/AppProvider';
 import {Bridge, Buy, Faq, Login, ProtectedRoute, Terms} from './routes';
+import {openInNewTab} from './utils';
 
 export const App = () => {
+  const [trackDiscordClick] = useTracking(TrackEvent.DISCORD_TAB_CLICK);
+  const {DISCORD_LINK_URL} = useConstants();
   const {isAcceptTerms} = useApp();
   const {pathname} = useLocation();
   const {isLoggedIn} = useLogin();
   const buyProviders = useBuyProviders();
+  const isBridgeRoute = !['/terms', '/faq'].includes(pathname);
+
+  const onDiscordClick = () => {
+    trackDiscordClick();
+    openInNewTab(DISCORD_LINK_URL);
+  };
 
   return (
     <div className={styles.app}>
       <Header />
-      <StyledBackground withLightAccent={!['/terms', '/faq'].includes(pathname)}>
+      <StyledBackground withLightAccent={isBridgeRoute}>
         <Routes>
           <Route
             element={
@@ -33,6 +45,7 @@ export const App = () => {
           <Route element={<Navigate replace to="/" />} path="*" />
         </Routes>
       </StyledBackground>
+      {isBridgeRoute && <SideButton icon={<DiscordIcon />} onClick={onDiscordClick} />}
       <Footer />
     </div>
   );
