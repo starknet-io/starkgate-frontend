65e0a3dbae8143b88da3a5cf203da235ee4f3e44
Dan Ziv <danz@starkware.co>
2022-05-24 15:00:15 +0300

    fix

--- src/components/UI/FullScreenContainer/FullScreenContainer.js
+++ src/components/UI/FullScreenContainer/FullScreenContainer.js
@@ -1,12 +1,19 @@
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, {forwardRef} from 'react';
 
 import styles from './FullScreenContainer.module.scss';
 
-export const FullScreenContainer = ({children}) => {
-  return <div className={styles.fullScreenContainer}>{children}</div>;
-};
+export const FullScreenContainer = forwardRef((props, ref) => {
+  return (
+    <div ref={ref} className={styles.fullScreenContainer} {...props}>
+      {props.children}
+    </div>
+  );
+});
+
+FullScreenContainer.displayName = 'FullScreenContainer';
 
 FullScreenContainer.propTypes = {
+  onScroll: PropTypes.func,
   children: PropTypes.oneOfType([PropTypes.object, PropTypes.array])
 };
