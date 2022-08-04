aa2f451882722413cb0236d6b9ab8a6a4532431d
Gal Ron Uzan <galu@starkware.co>
2022-08-04 14:50:20 +0300

    comments issues

--- src/components/UI/EthereumWalletButton/EthereumWalletButton.js
+++ src/components/UI/EthereumWalletButton/EthereumWalletButton.js
@@ -1,65 +1,24 @@
-import {ChainInfo, WalletStatus} from '@starkware-industries/commons-js-enums';
-import React, {useEffect} from 'react';
+import {ChainInfo, NetworkType} from '@starkware-industries/commons-js-enums';
 
 import {useEnvs} from '../../../hooks';
-import {useApp} from '../../../providers/AppProvider';
-import {useMenu} from '../../../providers/MenuProvider';
 import {useIsL1} from '../../../providers/TransferProvider';
 import {useL1Wallet} from '../../../providers/WalletsProvider';
-import {WalletButton} from '../index';
+import {NetworkWalletButton} from '../index';
 
 export const EthereumWalletButton = () => {
   const {account, config, error, status} = useL1Wallet();
   const {SUPPORTED_L1_CHAIN_ID} = useEnvs();
-  const {navigateToRoute} = useApp();
-  const {showAccountMenu} = useMenu();
   const [, swapToL1] = useIsL1();
 
-  const networkName = 'Ethereum';
-
-  useEffect(() => {
-    error && handleWalletError(error);
-  }, [error]);
-
-  const handleWalletButtonClick = () => {
-    switch (status) {
-      case WalletStatus.DISCONNECTED:
-        return handleConnectWallet();
-      case WalletStatus.CONNECTING:
-        return handleConnectingWallet();
-      case WalletStatus.CONNECTED:
-        return handleConnectedWallet();
-      default:
-        break;
-    }
-  };
-
-  const handleConnectedWallet = () => {
-    swapToL1();
-    showAccountMenu();
-    navigateToRoute('/');
-  };
-
-  const handleConnectWallet = () => {
-    // TODO - will display the login modal
-  };
-
-  const handleConnectingWallet = () => {
-    // TODO
-  };
-
-  const handleWalletError = () => {
-    // TODO
-  };
-
   return (
-    <WalletButton
+    <NetworkWalletButton
       account={account}
       chain={ChainInfo.L1[SUPPORTED_L1_CHAIN_ID].NAME}
+      error={error}
       logoPath={config?.logoPath || ''}
-      network={networkName}
+      network={NetworkType.L1}
       status={status}
-      onClick={handleWalletButtonClick}
+      swapFn={swapToL1}
     />
   );
 };
--- src/components/UI/StarknetWalletButton/StarknetWalletButton.js
+++ src/components/UI/StarknetWalletButton/StarknetWalletButton.js
@@ -1,63 +1,25 @@
-import {ChainInfo, WalletStatus} from '@starkware-industries/commons-js-enums';
-import React, {useEffect} from 'react';
+import {ChainInfo, NetworkType} from '@starkware-industries/commons-js-enums';
+import React from 'react';
 
 import {useEnvs} from '../../../hooks';
-import {useApp} from '../../../providers/AppProvider';
-import {useMenu} from '../../../providers/MenuProvider';
 import {useIsL2} from '../../../providers/TransferProvider';
 import {useL2Wallet} from '../../../providers/WalletsProvider';
-import {WalletButton} from '../index';
+import {NetworkWalletButton} from '../index';
 
 export const StarknetWalletButton = () => {
   const {account, config, error, status} = useL2Wallet();
   const {SUPPORTED_L2_CHAIN_ID} = useEnvs();
-  const networkName = 'Starknet';
-  const {navigateToRoute} = useApp();
-  const {showAccountMenu} = useMenu();
   const [, swapToL2] = useIsL2();
 
-  useEffect(() => {
-    error && handleWalletError(error);
-  }, [error]);
-
-  const handleWalletButtonClick = () => {
-    switch (status) {
-      case WalletStatus.DISCONNECTED:
-        return handleConnectWallet();
-      case WalletStatus.CONNECTING:
-        return handleConnectingWallet();
-      case WalletStatus.CONNECTED:
-        return onL2WalletButtonClick();
-      default:
-        break;
-    }
-  };
-
-  const onL2WalletButtonClick = () => {
-    swapToL2();
-    showAccountMenu();
-    navigateToRoute('/');
-  };
-
-  const handleConnectWallet = () => {
-    // TODO - will display the login modal
-  };
-
-  const handleConnectingWallet = () => {
-    // TODO
-  };
-
-  const handleWalletError = () => {
-    // TODO
-  };
   return (
-    <WalletButton
+    <NetworkWalletButton
       account={account}
       chain={ChainInfo.L2[SUPPORTED_L2_CHAIN_ID].NAME}
+      error={error}
       logoPath={config?.logoPath || ''}
-      network={networkName}
+      network={NetworkType.L2}
       status={status}
-      onClick={handleWalletButtonClick}
+      swapFn={swapToL2}
     />
   );
 };
