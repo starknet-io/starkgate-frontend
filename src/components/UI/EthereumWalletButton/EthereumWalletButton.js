ae2c54d23e5091756db8168152dfb462a5245b44
galuzan-starkware <105715230+galuzan-starkware@users.noreply.github.com>
2022-08-07 10:04:06 +0300

    feat(3015851363): create and display the header's wallet connect buttons (#254)
    * prerelease: v1.8.0-dev.2
    
    * galu(3015821438): change location of transfer btn
    
    * fix styles issues
    
    * feat(3015851363): create-buttons-components
    
    * feat(3015851363): create and display wallet btns
    
    * enums changes
    
    * linting
    
    * comments issues
    
    Co-authored-by: yairans-starkware <yaira@starkware.co>
    Co-authored-by: DanZ <danz@starkware.co>

---
+++ src/components/UI/EthereumWalletButton/EthereumWalletButton.js
@@ -0,0 +1,24 @@
+import {ChainInfo, NetworkType} from '@starkware-industries/commons-js-enums';
+
+import {useEnvs} from '../../../hooks';
+import {useIsL1} from '../../../providers/TransferProvider';
+import {useL1Wallet} from '../../../providers/WalletsProvider';
+import {NetworkWalletButton} from '../index';
+
+export const EthereumWalletButton = () => {
+  const {account, config, error, status} = useL1Wallet();
+  const {SUPPORTED_L1_CHAIN_ID} = useEnvs();
+  const [, swapToL1] = useIsL1();
+
+  return (
+    <NetworkWalletButton
+      account={account}
+      chain={ChainInfo.L1[SUPPORTED_L1_CHAIN_ID].NAME}
+      error={error}
+      logoPath={config?.logoPath || ''}
+      network={NetworkType.L1}
+      status={status}
+      swapFn={swapToL1}
+    />
+  );
+};
---
+++ src/components/UI/NetworkWalletButton/NetworkWalletButton.js
@@ -0,0 +1,68 @@
+import {WalletStatus} from '@starkware-industries/commons-js-enums';
+import PropTypes from 'prop-types';
+import {useEffect} from 'react';
+
+import {useApp} from '../../../providers/AppProvider';
+import {useMenu} from '../../../providers/MenuProvider';
+import {WalletButton} from '../index';
+
+export const NetworkWalletButton = ({account, chain, logoPath, network, status, error, swapFn}) => {
+  const {navigateToRoute} = useApp();
+  const {showAccountMenu} = useMenu();
+
+  useEffect(() => {
+    error && handleWalletError(error);
+  }, [error]);
+
+  const handleWalletButtonClick = () => {
+    switch (status) {
+      case WalletStatus.DISCONNECTED:
+        return handleConnectWallet();
+      case WalletStatus.CONNECTING:
+        return handleConnectingWallet();
+      case WalletStatus.CONNECTED:
+        return handleConnectedWallet();
+      default:
+        break;
+    }
+  };
+
+  const handleConnectedWallet = () => {
+    swapFn();
+    showAccountMenu();
+    navigateToRoute('/');
+  };
+
+  const handleConnectWallet = () => {
+    // TODO - will display the login modal
+  };
+
+  const handleConnectingWallet = () => {
+    // TODO
+  };
+
+  const handleWalletError = () => {
+    // TODO
+  };
+
+  return (
+    <WalletButton
+      account={account}
+      chain={chain}
+      logoPath={logoPath}
+      network={network}
+      status={status}
+      onClick={handleWalletButtonClick}
+    />
+  );
+};
+
+NetworkWalletButton.propTypes = {
+  account: PropTypes.string,
+  chain: PropTypes.string,
+  logoPath: PropTypes.string,
+  network: PropTypes.string,
+  status: PropTypes.string,
+  error: PropTypes.string,
+  swapFn: PropTypes.func
+};
