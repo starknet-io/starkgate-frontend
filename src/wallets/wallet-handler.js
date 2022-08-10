793f4bbd5fcb4f238ba0f9670bb6288c93e71caa
Dan Ziv <danz@starkware.co>
2022-08-10 14:53:28 +0300

    3054011722

--- src/wallets/wallet-handler.js
+++ src/wallets/wallet-handler.js
@@ -1,6 +1,17 @@
+import strings from '../config/strings';
+import {getPropertyPath, isChrome, isFirefox} from '../utils';
+
 export class WalletHandler {
   constructor(config) {
     this.config = config;
+  }
+
+  isBrowserSupported() {
+    const isBrowserSupported = isChrome() || isFirefox();
+    if (!isBrowserSupported) {
+      throw new Error(getPropertyPath(strings, 'menus.login.unsupportedBrowserTxt'));
+    }
+    return true;
   }
 
   isInstalled() {
