b912b189384fdbf3330e4e7530f5b226d083f3ce
Dan Ziv <danz@starkware.co>
2022-08-12 01:01:55 +0300

    3076582507

---
+++ src/components/UI/Alert/Alert.js
@@ -0,0 +1,49 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+
+import {ReactComponent as InfoIcon} from '../../../assets/svg/icons/info.svg';
+import {ReactComponent as SuccessIcon} from '../../../assets/svg/icons/success.svg';
+import {ReactComponent as WarningIcon} from '../../../assets/svg/icons/warning.svg';
+import {toClasses} from '../../../utils';
+import styles from './Alert.module.scss';
+
+export const AlertType = {
+  SUCCESS: 'success',
+  ERROR: 'error',
+  WARNING: 'warning',
+  INFO: 'info'
+};
+
+export const Alert = ({
+                        title ='',
+                        message ='',
+                        type = AlertType.INFO
+                      }) => {
+  const renderIcon = () => {
+    switch (type) {
+      case AlertType.SUCCESS:
+        return <SuccessIcon />;
+      case AlertType.WARNING:
+        return <WarningIcon />;
+      case AlertType.ERROR:
+      case AlertType.INFO:
+        return <InfoIcon />;
+    }
+  };
+
+  return (
+    <div className={toClasses(styles.alert, styles[type])}>
+      <div className={styles.icon}>{renderIcon()}</div>
+      <div className={styles.text}>
+        {title && <div className={styles.title}>{title}</div>}
+        {message && <div className={styles.message}>{message}</div>}
+      </div>
+    </div>
+  );
+};
+
+Alert.propTypes = {
+  title: PropTypes.string,
+  message: PropTypes.string,
+  type: PropTypes.string
+};
