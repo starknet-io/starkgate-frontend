76f5323adc9494852e76c5ffadd1fa092cd6b6f1
yairans-starkware <yaira@starkware.co>
2022-08-15 18:01:15 +0300

    remove from route/index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,5 @@
 export * from './Bridge/Bridge';
 export * from './Terms/Terms';
-export * from './Login/Login';
 export * from './Faq/Faq';
 export * from './Liquidity/Liquidity';
 export * from './ProtectedRoute';
