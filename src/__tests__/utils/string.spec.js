8fe79791b89fa7aaf785a191cc37928356bbd54c
Dan Ziv <danz@starkware.co>
2022-09-01 14:00:58 +0300

    Remove tests

--- src/__tests__/utils/string.spec.js
+++
@@ -1,8 +0,0 @@
-import {capitalize} from '@starkware-industries/commons-js-utils';
-
-describe('capitalize', () => {
-  it('should capitalize string', () => {
-    expect(capitalize('hello')).toEqual('Hello');
-    expect(capitalize('some message')).toEqual('Some message');
-  });
-});
