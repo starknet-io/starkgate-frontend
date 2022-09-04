b2a24061439bf9d53b848cf1b302e6d0a386fade
Gal Ron Uzan <galu@starkware.co>
2022-09-04 17:07:27 +0300

    format

--- src/App.js
+++ src/App.js
@@ -41,7 +41,7 @@ export const App = () => {
           <Route element={<Navigate replace to="/" />} path="*" />
         </Routes>
       </StyledBackground>
-      <SideButton icon={<DiscordIcon width={26} height={98} />} onClick={onDiscordClick} />
+      <SideButton icon={<DiscordIcon height={98} width={26} />} onClick={onDiscordClick} />
     </div>
   );
 };
