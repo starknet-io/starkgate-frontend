a6fa23f8b2f40c7f3691cd77ca988601fd657b71
DanZ <danz@starkware.co>
2022-09-15 18:21:16 +0300

    fix(3217431965): Header responsiveness is messed up (#316)

--- src/components/UI/WalletButton/WalletButton.js
+++ src/components/UI/WalletButton/WalletButton.js
@@ -1,13 +1,17 @@
 import {WalletStatus} from '@starkware-industries/commons-js-enums';
-import {evaluate, shortenAddress, toClasses} from '@starkware-industries/commons-js-utils';
+import {evaluate, shortenAddress} from '@starkware-industries/commons-js-utils';
 import PropTypes from 'prop-types';
 import React from 'react';
-import useBreakpoint from 'use-breakpoint';
 
-import {Breakpoint, isDesktop, isMobile, isMobileOrTablet} from '../../../enums';
 import {useColors, useHeaderTranslation} from '../../../hooks';
 import {Button, DynamicIcon} from '../index';
 import styles from './WalletButton.module.scss';
+
+export const WalletButtonIconSize = {
+  SMALL: 10,
+  MEDIUM: 20,
+  LARGE: 30
+};
 
 export const WalletButton = ({account, chain, network, logoPath, status, onClick}) => {
   return status === WalletStatus.CONNECTED ? (
@@ -27,42 +31,19 @@ WalletButton.propTypes = {
 };
 
 const AccountWalletButton = ({account, chain, logoPath, onClick}) => {
-  const {breakpoint} = useBreakpoint(Breakpoint);
   const {colorOrangeSoda, colorWhiteOp10, colorWhite, colorDarkBlueGray} = useColors();
   const {accountWalletBtnTxt} = useHeaderTranslation();
 
-  const WALLET_LOGO_SIZE = 20;
-  const WALLET_LOGO_SIZE_MOBILE = 40;
-
-  const getText = () => {
-    const address = shortenAddress(account);
-    if (isMobileOrTablet(breakpoint)) {
-      return '';
-    } else if (!isDesktop(breakpoint)) {
-      return address;
-    }
-    return evaluate(accountWalletBtnTxt, {address});
-  };
-
-  const renderChainLabel = () => {
-    return isDesktop(breakpoint) ? <ChainLabel chain={chain} /> : null;
-  };
-
-  const renderWalletLogo = () => {
-    const logoSize = isMobile(breakpoint) ? WALLET_LOGO_SIZE_MOBILE : WALLET_LOGO_SIZE;
-    return <DynamicIcon path={logoPath} size={logoSize} />;
-  };
-
   return (
     <Button
-      className={toClasses(styles.accountWalletButton, styles[breakpoint.toLowerCase()])}
+      className={styles.accountWalletButton}
       colorBackground={colorWhiteOp10}
       colorBackgroundHover={colorDarkBlueGray}
       colorBorder={colorOrangeSoda}
       colorText={colorWhite}
-      iconLeft={renderWalletLogo()}
-      iconRight={renderChainLabel()}
-      text={getText()}
+      iconLeft={<DynamicIcon path={logoPath} size={WalletButtonIconSize.MEDIUM} />}
+      iconRight={<ChainLabel chain={chain} />}
+      text={evaluate(accountWalletBtnTxt, {address: shortenAddress(account)})}
       onClick={onClick}
     />
   );
